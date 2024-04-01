6b460cf34361551715aaa69261c5b5ad50109681
OsinkinaVV <is25osinkinavv@artcollege.ru>
2024-04-01 13:25:56 +0300

    «Вывели результат вычисления на экран.».

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ namespace ConsoleApp14
             Console.Write( "Введите значения параметра x = ");
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
+
+            double F = Math.Cos(x) + Math.Cos(x);
+
+            Console.Write($"Для значен x = {x}, F = {F} ");
+            Console.Write("Осинкна ВВ 31ИС");
         }
     }
 }
